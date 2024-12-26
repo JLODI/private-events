ece81dace8127fa31a9b8790441d435319d582ac
JLODI <88990920+JLODI@users.noreply.github.com>
2024-12-26 15:28:08 -0800

    added create and new methods in events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,23 @@
 class EventsController < ApplicationController
     def index
         @events = Event.all
-      end
+    end
+    def new
+        @event = Event.new
+    end
+    def create
+        @event = current_user.events.new(event_params)
+        if @event.save
+            flash[:notice] = "Your Event has been posted!"
+            redirect_to events_index
+        else
+            flash.now[:error] = @event.errors.full_messages.join ('<br/>')
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+private
+    def event_params
+        params.expect(event: [:name, :date, :desc])
+    end
 end
