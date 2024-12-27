3f8565d63c3c1de0ced329a83a9e0f00368c5743
JLODI <88990920+JLODI@users.noreply.github.com>
2024-12-27 14:09:52 -0800

    added show methods and view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,9 @@ class EventsController < ApplicationController
     def index
         @events = Event.all
     end
+    def show
+        @event = Event.find(params[:id])
+    end
     def new
         @event = Event.new
     end
