694f8454282f4d2956569855ff3d52452662025e
JLODI <88990920+JLODI@users.noreply.github.com>
2024-12-27 14:42:32 -0800

    added user routes and show method and show view

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
         sessions: 'users/sessions'
       }
+  resources :users, only: [:show]
   resources :events
   root "events#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
