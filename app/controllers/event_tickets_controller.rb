b4e3ba68081107cc30c3c15a32aaa8a367d8a8eb
JLODI <88990920+JLODI@users.noreply.github.com>
2024-12-28 02:12:16 -0800

    added create event tickets / attendance methods in controller, added attend the event in event show

--- app/controllers/event_tickets_controller.rb
+++ app/controllers/event_tickets_controller.rb
@@ -1,15 +1,22 @@
 class EventTicketsController < ApplicationController
     def new
-        @event_ticket = Event.find(params[:id])
+        @event_ticket = EventTicket.new
     end
     def create
-        @event_ticket = Event.find(params[:id])
-        @event_ticket.attendees << current_user
+        @event_ticket = EventTicket.new(event_ticket_params)
         if @event_ticket.save
             flash[:notice] = "You are going to the event!"
-            redirect_to users_path
+            redirect_to event_path(:id)
         else
             flash.now[:error] = "Something went wrong, please try again."
         end
+    end
+
+    def show
+        @event_ticket = Event.find(params[:attended_event_id])
+    end
+    private
+    def event_ticket_params
+        params.expect(event_ticket:[:attendee_id, :attended_event_id])
     end
 end
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,13 +1,16 @@
 class EventsController < ApplicationController
     
-    before_action :authenticate_user!, except: [:index]
+    before_action :authenticate_user!
 
     def index
         @events = Event.all
     end
+
     def show
         @event = Event.find(params[:id])
+        @event_ticket = EventTicket.new(attendee_id: current_user.id, attended_event_id: @event.id)
     end
+
     def new
         @event = Event.new
     end
