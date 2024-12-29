34255c1d67fe7e6f035ebe0d51b90f88bb022642
JLODI <88990920+JLODI@users.noreply.github.com>
2024-12-28 19:56:15 -0800

    added past and future events in users controller and view and events controller and view, used scope in events

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,7 @@ class UsersController < ApplicationController
         @user = User.find(params[:id])
         @created_events = @user.created_events
         @attended_events = @user.attended_events
+        @past_events = @attended_events.where("date < ?", Date.today)
+        @future_events = @attended_events.where("date >= ?", Date.today).order(date: :asc)
     end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,6 @@ class Event < ApplicationRecord
     belongs_to :creator, class_name: "User"
     has_many :event_tickets, foreign_key: "attended_event_id"
     has_many :attendees, through: :event_tickets
+    scope :past, -> { where("date < ?", Date.today)}
+    scope :future, -> { where("date >= ?", Date.today)}
 end
