dfeb656939cf492f83488b73c4087a87ac3153b8
JLODI <88990920+JLODI@users.noreply.github.com>
2024-12-30 18:26:45 -0800

    added dependent destroy on user created events

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :created_events, foreign_key: "creator_id", class_name: "Event"
+  has_many :created_events, foreign_key: "creator_id", class_name: "Event", dependent: :destroy
   has_many :event_tickets, foreign_key: "attendee_id"
   has_many :attended_events, through: :event_tickets, class_name: "Event"
 end
