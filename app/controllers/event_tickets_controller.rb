2a35a2737aac4a9a420562b44bca6f9a4b937e0f
JLODI <88990920+JLODI@users.noreply.github.com>
2024-12-31 19:27:16 -0800

    working on not attending function for users, in progress

--- app/controllers/event_tickets_controller.rb
+++ app/controllers/event_tickets_controller.rb
@@ -15,6 +15,20 @@ class EventTicketsController < ApplicationController
     def show
         @event_ticket = Event.find(params[:attended_event_id])
     end
+
+    def destroy
+        @event_ticket = EventTicket.where(attended_event_id: params[:event_id], attendee_id: current_user.id)
+        @event_ticket = @event_ticket.first
+        @event_ticket.destroy
+            if @event_ticket.first.destroy
+                flash[:notice] = "You are no longer attending the event."
+                redirect_to event_path(params[:attended_event_id])
+            else
+                flash.now[:error] = @event_ticket.errors.full_messages.join ('<br/>')
+                render :destroy, status: :unprocessable_entity
+            end
+    end  
+
     private
     def event_ticket_params
         params.expect(event_ticket:[:attendee_id, :attended_event_id])
